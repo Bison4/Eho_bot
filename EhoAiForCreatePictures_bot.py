8c77739d8772758bca5a6c9f4df109aac448d7b7
NIger <you@example.com>
2025-04-27 17:32:11 +0300

    2.1 commit

--- EhoAiForCreatePictures_bot.py
+++ EhoAiForCreatePictures_bot.py
@@ -5,10 +5,10 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(func = lambda message:True)
 def ehno_call(message):
     msg_text = message.text
-    if msg_text =="Удприть ребенка негра🥰" or msg_text =="Ударить негра😂":
+    if msg_text =="Удприть ребенка негра🥰":
         bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_back)
-    elif msg_text =="Назад":
-        bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_start)
+    elif  msg_text =="Ударить негра😂":
+        bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_back)
     else :
         bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_2)
 
--- keybords.py
+++ keybords.py
@@ -1,13 +1,11 @@
 import telebot
 
-button_start = telebot.types.KeyboardButton(text= 'Начнем бить негров?🤣')
 button_pictures = telebot.types.KeyboardButton(text= 'Ударить негра😂')
 button_text = telebot.types.KeyboardButton(text= 'Удприть ребенка негра🥰')
 button_back = telebot.types.KeyboardButton(text= 'Назад')
 
-keybord_start = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+
 keybord_2 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 keybord_back = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-keybord_start.add(button_start)
 keybord_2.add(button_pictures,button_text)
 keybord_back.add(button_back)
