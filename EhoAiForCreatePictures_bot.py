e9702dbceff89f899c281321f8d90c7635b5522d
NIger <you@example.com>
2025-04-27 17:29:20 +0300

    2commits

--- EhoAiForCreatePictures_bot.py
+++ EhoAiForCreatePictures_bot.py
@@ -1,10 +1,17 @@
-import telebot
+import telebot,keybords
 TOKEN = '7931226849:AAHRaUKMoSaD2YjwovnvBlqVZP0tcgCuCRc'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func = lambda message:True)
 def ehno_call(message):
-    bot.send_message(message.chat.id,message.text)
+    msg_text = message.text
+    if msg_text =="Удприть ребенка негра🥰" or msg_text =="Ударить негра😂":
+        bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_back)
+    elif msg_text =="Назад":
+        bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_start)
+    else :
+        bot.send_message(message.chat.id,text='1',reply_markup=keybords.keybord_2)
+
 
 
 bot.polling()
