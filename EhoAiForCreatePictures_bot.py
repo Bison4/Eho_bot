33c6fc013f71326f42cf36025c484d4a2cc7f203
NIger <you@example.com>
2025-05-25 17:36:54 +0300

    4commit

--- EhoAiForCreatePictures_bot.py
+++ EhoAiForCreatePictures_bot.py
@@ -1,7 +1,11 @@
 import telebot,keybords
 import fsm
+import ai
 TOKEN = '7931226849:AAEyPiV7BdqT6iiwBb7AjQbkcIp9FuHYJis'
+# https://api.telegram.org/bot7931226849:AAEyPiV7BdqT6iiwBb7AjQbkcIp9FuHYJis/deleteWebhook
+
 stater = fsm.FMS()
+ai_servise = ai.AI()
 bot = telebot.TeleBot(TOKEN)
 def main_menu(message):
         if message.text =="Генерация изображения 🌅":
@@ -18,12 +22,21 @@ def image_menu(message):
         if message.text == "Назад↩":
             back_main_menu(message)
         else:
-            bot.send_message(message.chat.id,text='Окак , делаю ...🛠')            
+            try:
+                msg = bot.send_message(message.chat.id,text='Окак , делаю ...🛠')      
+                image_url = ai_servise.generate_image(message.text)
+                bot.delete_message(chat_id=message.chat.id,message_id=msg.id)
+                bot.send_photo(chat_id=message.chat.id,caption="Держи фото🦾",photo=image_url)
+            except Exception as e:
+                bot.send_message(message.chat.id,text=f'Окак ,EEErOo0r({str(e)})')      
 def text_menu(message):
         if message.text == "Назад↩":
+            ai_servise.clear_dialog(message.chat.id)
             back_main_menu(message)
         else:
-            bot.send_message(message.chat.id,text='Окак , делаю ...🛠')            
+            msg = bot.send_message(message.chat.id,text='Окак , делаю ...🛠')   
+            txt = ai_servise.genrate_text(message.text,message.chat.id)     
+            msg = bot.edit_message_text(text = txt,chat_id= message.chat.id,message_id=msg.id)    
 def back_main_menu(message):
     stater.set_state(message.chat.id, fsm.DEFAULT_STATE)
     bot.send_message(message.chat.id,text='Главное меню🤯',reply_markup=keybords.keybord_2)
